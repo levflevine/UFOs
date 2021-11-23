40a7893934f74ce509e2a5c3c5f7a1c22425948d
levflevine <levflevine@gmail.com>
2021-11-22 22:02:42 -0500

    complete filtered table data

--- static/js/app.js
+++ static/js/app.js
@@ -42,3 +42,8 @@ function buildTable(data) {
     // just be the original tableData.
     buildTable(filteredData);
   };
+
+  d3.selectAll("#filter-btn").on("click", handleClick);
+
+  buildTable(tableData);
+  
